044fc3fc31cbbc6a8816de7a10600f2010170d2e
Sean <sgilmore@michael-sean.com>
2017-10-10 13:43:01 -0400

    updated title on index page, changes to _form.css and EditUserAccount and UserAccount

--- client/src/components/UserAccount.js
+++ client/src/components/UserAccount.js
@@ -3,6 +3,44 @@ import { Link, Redirect } from 'react-router-dom'
 import axios from 'axios'
 
 class UserAccount extends Component {
+  constructor(){
+    super();
+    this.state = {
+      user: {}
+    }
+  }
+
+  componentWillMount(){
+    this._fetchCurrentUser();
+  }
+
+  _fetchCurrentUser = async () => {
+    try {
+    const res = await axios.get('/api/users');
+    this.setState({user: res.data})
+    console.log(res.data)
+    } 
+    catch(err) {
+      console.log(err)
+    }
+  }
+
+  _deleteCurrentUser = async () => {
+    try {
+      const res = await axios.delete('/auth');
+      this._logout();
+      return res.data
+    } 
+    catch(err) {
+      console.log(err);
+    }
+  }
+
+  _logOut = async () => {
+    console.log("CLICK");
+    const response = await axios.delete("/auth/sign_out");
+  }
+
   render () {
     if (!localStorage['access-token']) {
       return (<Redirect to='/' />)
